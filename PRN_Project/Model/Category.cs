96d59fc40a3116afe12c8d683d6a14b87f51fc9e
Nguyễn Minh Quân <99967044+miquanajoy@users.noreply.github.com>
2022-10-18 01:05:08 +0700

    fix

--- PRN_Project/Model/Category.cs
+++ PRN_Project/Model/Category.cs
@@ -1,8 +1,12 @@
-﻿namespace PRN_Project.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PRN_Project.Model
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
