9c97e9dcb737e05edbf083ae587f966de70bb4bf
Nguyễn Minh Quân <99967044+miquanajoy@users.noreply.github.com>
2022-10-18 01:14:22 +0700

    update dbcontext on program.cs

---
+++ PRN_Project/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PRN_Project.Model;
+
+namespace PRN_Project.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Category { get; set; }
+    }
+}
