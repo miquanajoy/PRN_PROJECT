ccce75d2f99e476be07eec445c776bb0468a6f9b
Nguyễn Minh Quân <99967044+miquanajoy@users.noreply.github.com>
2022-10-19 13:16:28 +0700

    create category done

--- PRN_Project/Pages/Categories/Create.cshtml.cs
+++ PRN_Project/Pages/Categories/Create.cshtml.cs
@@ -1,16 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PRN_Project.Data;
 using PRN_Project.Model;
 
 namespace PRN_Project.Pages.Categories
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext dbContext;
 
-        public Category categories { get; set; }
+        public Category Category { get; set; }
 
         public void OnGet()
         {
+
+        }
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            dbContext = db;
+        }
+
+        public async Task<IActionResult> OnPost(Category category)
+        {
+            await dbContext.Category.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
