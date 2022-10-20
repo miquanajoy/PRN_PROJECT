ac3bfae2b9c3bf8173150f1581f2c13f4bc6d68f
Nguyễn Minh Quân <phucnguyentt32@gmail.com>
2022-10-20 13:43:39 +0700

    Update IRepository.cs

--- BookStore.DataAccess/Repository/IRepository/IRepository.cs
+++ BookStore.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,10 @@ namespace BookStore.DataAccess.Repository.IRepository
 {
     public  interface IRepository<T> where T : class
     {
-
+        void add(T entity); 
+        void remove(T entity);
+        void removeRange(IEnumerable<T> entity);
+        IEnumerable<T> GetAll();
+        T GetFirstOrDefault(Expression<Func<T, bool>>? filter=null);
     }
 }
