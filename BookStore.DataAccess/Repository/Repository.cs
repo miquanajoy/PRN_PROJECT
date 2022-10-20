f5cbc3963cc8cd657c6208f5f7ad7f34aa6649f4
Nguyễn Minh Quân <phucnguyentt32@gmail.com>
2022-10-20 14:03:42 +0700

    implement repository

---
+++ BookStore.DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using BookStore.DataAccess.Data;
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            dbContext = db;
+            this.dbSet = db.Set<T>();
+        }
+        public void add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>>? filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void removeRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+
+        }
+    }
+}
