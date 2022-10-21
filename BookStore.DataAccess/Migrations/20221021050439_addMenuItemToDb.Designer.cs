424f1b17a05e3e2efb067d1f3b9e0fa9eea0dc6a
Nguyễn Minh Quân <phucnguyentt32@gmail.com>
2022-10-21 19:52:26 +0700

    load db & add "edit, delete" button

--- BookStore.DataAccess/Migrations/20221021050439_addMenuItemToDb.Designer.cs
+++
@@ -1,1 +0,0 @@
-                    b.HasIndex("bookTypeId");
--- PRN_Project/wwwroot/js/menuItem.js
+++ PRN_Project/wwwroot/js/menuItem.js
@@ -1,3 +1,30 @@
 ﻿$(document).ready(function () {
-    $('#DT_load').DataTable();
+    $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/MenuItem",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "price", "width": "10%" },
+            { "data": "category.name", "width": "10%" },
+            { "data": "bookType.name", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="w-75 bth-group">
+                            <a href="Admin/MenuItems/upsert?id=${data}"
+                                class="btn btn-success text-white mx-2">
+                                <i class="bi bi-pencil-square"></i></a>
+                            <a href="Admin/MenuItems/upsert?id=${data}"
+                                class="btn btn-danger text-white" mx-2>
+                                <i class="bi bi-trash-fill"></i></a>
+                            </div>`
+                },
+                "width": "10%"
+            }
+        ],
+        "width" : "100%"
+    });
 });
