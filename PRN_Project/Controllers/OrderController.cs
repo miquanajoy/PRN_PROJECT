0e7a688c60fa41dc7f0c5dfb284b8f33f98a8b5d
DThongLeon <hoangthong432001@gmail.com>
2022-11-01 04:11:24 +0700

    add order details and order list

---
+++ PRN_Project/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PRN_Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            var OrderHeaderList = _unitOfWork.OrderHeader.getAll(includeProperties:"ApplicationUser");
+            return Json(new {data = OrderHeaderList});
+        }
+
+        
+    }
+}
---
+++ PRN_Project/wwwroot/js/orderList.js
@@ -0,0 +1,63 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+   dataTable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "/api/order",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "id", "width": "25%" },
+            { "data": "pickUpName", "width": "10%" },
+            { "data": "applicationUser.email", "width": "10%" },
+            { "data": "orderTotal", "width": "10%" },
+            { "data": "pickUpItem", "width": "10%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="w-75 bth-group">
+                            <a href="/Admin/Order/OrderDetails?id=${data}"
+                                class="btn btn-success text-white mx-2">
+                                <i class="bi bi-pencil-square"></i></a>
+                            
+                            </div>`
+                },
+                "width": "10%"
+            }
+        ],
+        "width" : "100%"
+    });
+});
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        dataTable.ajax.reload();
+                        //successNoti
+                        toastr.success(data.message);
+                    }
+                    else {
+                        //failNoti
+                        toastr.error(data.message);
+
+                    }
+                }
+            })
+        }
+    })
+
+}
