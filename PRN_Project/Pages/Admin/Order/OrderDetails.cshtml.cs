2b6d1e0fd9170d36b7dfb14c8680cab8bd1d6147
Nhat-An-Pham <phamnhatan01@gmail.com>
2022-11-02 05:19:29 +0700

    add OrderDetails and OrderDetailsViewModel

--- PRN_Project/Pages/Admin/Order/OrderDetails.cshtml.cs
+++ PRN_Project/Pages/Admin/Order/OrderDetails.cshtml.cs
@@ -1,3 +1,6 @@
+using BookStore.DataAccess.Repository.IRepository;
+using BookStore.Models;
+using BookStore.Models.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +8,22 @@ namespace PRN_Project.Pages.Admin.Order
 {
     public class OrderDetailsModel : PageModel
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderDetailsVM OrderDetailsVM { get; set; }
+
+        public OrderDetailsModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public void OnGet(int id)
         {
+            OrderDetailsVM = new()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.getFirstOrDefault(u => u.Id == id, includeProperties: "ApplicationUser"),
+                OrderDetails = _unitOfWork.OrderDetails.getAll(u => u.OrderId == id).ToList()
+            };
         }
     }
 }
